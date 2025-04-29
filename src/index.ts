2b5e5f6049881800deb298b79a0fa0b58d9127e0
Patricia <englishwithpatricia007@gmail.com>
2025-04-29 09:34:14 -0300

    Vendo tipos de variáveis do TS

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,39 @@ idk = '12'
 idk = true;
 
 const ids: number[] = [1, 2, 3, 4, 5];
+const booleans: boolean[] = [true, false, true];
+const names: string[] = ['Lucas', 'João', 'Maria'];
 
-console.log(age);
+//Tupla
+const person: [string, number] = ['Lucas', 5];
+
+//Lista de tuplas
+const people: [string, number][] = [
+  ['Lucas', 5],
+  ['João', 10],
+  ['Maria', 15]
+];
+
+//Intersections
+const product: string | number | boolean = false;
+
+//Enum
+enum Direction {
+  Up = 1,
+  Down = 2,
+  Left = 'Esquerda',
+  Right = 'Direita'
+}
+
+let direction = Direction.Up;
+console.log(direction);
+direction = Direction.Left;
+console.log(direction);
+
+//Type Assertion
+const productName : any = 'Tênis';
+
+let itemId
+itemId = productName as string;
+let item2Id = <string>productName;
+
