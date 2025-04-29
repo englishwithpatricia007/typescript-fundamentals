8f8044df5a3719decf4102a3809564df70c92604
Patricia <englishwithpatricia007@gmail.com>
2025-04-29 08:56:16 -0300

    arquivo de configuracao to TS

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//Tipos básicos
+let age: number = 5;
+const firstName: string = "Lucas";
+const isValid: boolean = true;
+
+let idk: any = 5;
+idk = '12'
+idk = true;
+
+const ids: number[] = [1, 2, 3, 4, 5];
+
+console.log(age);
